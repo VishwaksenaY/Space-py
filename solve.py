3294de9ae63e0e4b256a5d3627694649d822c359
Vishwaksena Yedluri <182167886+VishwaksenaY@users.noreply.github.com>
2025-02-12 21:03:24 -0800

    Create solve.py

---
+++ solve.py
@@ -0,0 +1,19 @@
+import hashlib
+
+username_trial = b"PRITCHARD"
+
+key_part_static1_trial = "picoCTF{1n_7h3_|<3y_of_"
+key_part_dynamic1_trial = ""
+key_part_static2_trial = "}"
+
+hash = hashlib.sha256(username_trial).hexdigest()
+
+        if key[i] != hashlib.sha256(username_trial).hexdigest()[4]:
+        if key[i] != hashlib.sha256(username_trial).hexdigest()[5]:
+        if key[i] != hashlib.sha256(username_trial).hexdigest()[3]:
+        if key[i] != hashlib.sha256(username_trial).hexdigest()[6]:
+        if key[i] != hashlib.sha256(username_trial).hexdigest()[2]:
+        if key[i] != hashlib.sha256(username_trial).hexdigest()[7]:
+        if key[i] != hashlib.sha256(username_trial).hexdigest()[1]:
+        if key[i] != hashlib.sha256(username_trial).hexdigest()[8]:
+    
